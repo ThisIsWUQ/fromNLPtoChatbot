27eb651296ed221c240a93ebce7773b68a97b667
ThisIsWUQ <113877939+ThisIsWUQ@users.noreply.github.com>
2026-03-05 12:23:12 +0100

    Update st_generate_output.py

--- TermProject/src/st_generate_output.py
+++ TermProject/src/st_generate_output.py
@@ -14,7 +14,7 @@ from transformers import pipeline
 # Load an English language package
 
 @st.cache_resource
-nlp = en_core_web_lg.load()
+n = en_core_web_lg.load()
 
 # Set up working vocabularies
 
@@ -611,6 +611,7 @@ def st_generate_output_text(user_input, mode):
 
 
     return output
+
 
 
 
