8b6fe3b00e7908ed04497c5daf946d260fa1bc72
ThisIsWUQ <113877939+ThisIsWUQ@users.noreply.github.com>
2026-03-05 04:29:08 +0100

    Update st_generate_output.py

--- TermProject/src/st_generate_output.py
+++ TermProject/src/st_generate_output.py
@@ -9,9 +9,17 @@ import markovify
 from spacy.lang.en.stop_words import STOP_WORDS
 from tracery.modifiers import base_english
 from transformers import pipeline
+#from pathlib import Path
+
+import os, streamlit as st
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
+st.write("CWD:", os.getcwd())
+st.write("APP DIR:", Path(__file__).resolve().parent)
+st.write("DATA DIR exists?:", (Path(__file__).resolve().parent / "data").exists())
+st.write("DATA files:", list((Path(__file__).resolve().parent / "data").glob("*")))
+
+#BASE_DIR = Path(__file__).resolve().parent
 
 # Set up working vocabularies
 
@@ -612,6 +620,7 @@ def st_generate_output_text(user_input, mode):
 
 
     return output
+
 
 
 
