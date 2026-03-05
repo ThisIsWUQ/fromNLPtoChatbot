1978f90e1ee91792df30d458232dcab766fa2f71
ThisIsWUQ <113877939+ThisIsWUQ@users.noreply.github.com>
2026-03-05 14:09:05 +0100

    Update st_generate_output.py

--- TermProject/src/st_generate_output.py
+++ TermProject/src/st_generate_output.py
@@ -80,7 +80,6 @@ nlp = spacy.load('en_core_web_lg')
 
 # Load an emotion classifier model for English texts
 
-@st.cache_resource
 emotion_classifier = pipeline(
     "text-classification",
     model="j-hartmann/emotion-english-distilroberta-base",
@@ -610,6 +609,7 @@ def st_generate_output_text(user_input, mode):
 
 
     return output
+
 
 
 
